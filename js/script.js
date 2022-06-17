6604216c517f2d7d4567cb6f604f6bf223a05448
JacopoNa <j.nardelli@virgilio.it>
2022-06-17 15:40:35 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// CONSEGNA
+// definire un array di oggetti che rappresentano i membri del team.
+// Ogni membro ha le informazioni necessarie per stampare la relativa card: Nome, Ruolo e Foto.
+// Una volta definita la struttura dati, prendendo come riferimento la card di esempio presente nell'html,
+// stampare dinamicamente una card per ogni membro del team.
